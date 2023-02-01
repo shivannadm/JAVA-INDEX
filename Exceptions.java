b168fdd7ff3b4e135d4eec437e00af3f9f3e1702
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-02-01 22:26:25 +0530

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Exceptions {
+ public static void main(String[] args) { 
+ Scanner scan = new Scanner(System.in);
+int a,b,c;
+System.out.print("Enter the Two Interger Values\nA :");
+a=scan.nextInt();
+System.out.print("B :");
+b=scan.nextInt();
+scan.close();
+try {
+if(b==0)
+throw new ArithmeticException("Divide By Zero");
+c=a/b;
+System.out.println("\nThe Value of "+a+" / "+b+" is "+c);
+}catch(ArithmeticException e)
+{
+ System.out.println("Enter a non zero number for B!!!");
+}
+ }
+}
