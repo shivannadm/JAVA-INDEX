ef624722ead4087d52f9f99cf5c9753f184f0fae
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-02-03 23:05:43 +0530

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,56 @@
+package currencyconversion;
+
+import java.util.Scanner;
+
+public class Currency {
+    double inr, usd;
+    double euro, yen;
+    Scanner in = new Scanner(System.in);
+
+    public void dollartorupee() {
+        System.out.println("Enter dollars to convert into Rupees:");
+        usd = in.nextDouble();
+        inr = usd * 81.83;
+        System.out.println("Dollar =" + usd + " equal to INR=" + inr);
+        System.out.println("\n");
+    }
+
+    public void rupeetodollar() {
+        System.out.println("Enter Rupee to convert into Dollars:");
+        inr = in.nextDouble();
+        usd = inr / 81.83;
+        System.out.println("Rupee =" + inr + "equal to Dollars=" + usd);
+    }
+
+    public void eurotorupee() {
+        System.out.println("Enter Euro to convert into Rupees:");
+        euro = in.nextDouble();
+        inr = euro * 79.06;
+        System.out.println("Euro =" + euro + " equal to INR=" + inr);
+        System.out.println("\n");
+    }
+
+    public void rupeetoeuro() {
+        System.out.println("Enter Rupees to convert into Euro:");
+        inr = in.nextDouble();
+        euro = (inr / 79.06);
+        System.out.println("Rupee =" + inr + "equal to Euro=" + euro);
+        System.out.println("\n");
+    }
+
+    public void yentoruppe() {
+        System.out.println("Enter Yen to convert into Rupees:");
+        yen = in.nextDouble();
+        inr = yen * 0.57;
+        System.out.println("Yen =" + yen + " equal to INR=" + inr);
+        System.out.println("\n");
+    }
+
+    public void ruppetoyen() {
+        System.out.println("Enter Rupees to convert into Yen:");
+        inr = in.nextDouble();
+        yen = (inr / 0.57);
+        System.out.println("INR=" + inr + "equal to YEN" + yen);
+        System.out.println("\n");
+    }
+}
---
+++ Time.java
@@ -0,0 +1,36 @@
+package timeconversion;
+
+import java.util.Scanner;
+
+public class Time {
+    int hours, seconds, minutes;
+    Scanner in = new Scanner(System.in);
+
+    public void hourstominutes() {
+        System.out.println("Enter the no of Hours to convert into minutes");
+        hours = in.nextInt();
+        minutes = (hours * 60);
+        System.out.println("Minutes: " + minutes);
+    }
+
+    public void minutestohours() {
+        System.out.println("Enter the no of Minutes to convert into Hours");
+        minutes = in.nextInt();
+        hours = minutes / 60;
+        System.out.println("Hours: " + hours);
+    }
+
+    public void hourstoseconds() {
+        System.out.println("Enter the no of Hours to convert into Seconds");
+        hours = in.nextInt();
+        seconds = (hours * 3600);
+        System.out.println("Seconds: " + seconds);
+    }
+
+    public void secondstohours() {
+        System.out.println("Enter the no of Seconds to convert into Hours");
+        seconds = in.nextInt();
+        hours = seconds / 3600;
+        System.out.println(seconds + "seconds" + " is equal to " + hours + "hour");
+    }
+}
