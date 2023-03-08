c7cc354624c442402894415f6aeb3dc8185f1280
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-03-08 12:58:09 +0530

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -1,4 +1,4 @@
-package distanceconversion;
+package distanceconversion;  //It's only for crating package , The main function is uploaded once check it . File name is Conversios.java
 
 import java.util.Scanner;
 
