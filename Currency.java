8b857fdf96a301e6f550c2f113378ba9ea8bb1c5
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-03-08 12:57:50 +0530

    Update Currency.java

--- Currency.java
+++ Currency.java
@@ -1,4 +1,4 @@
-package currencyconversion;
+package currencyconversion; //It's only for crating package , The main function is uploaded once check it . File name is Conversios.java
 
 import java.util.Scanner;
 
