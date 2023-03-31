930f191966162dd251a10fd686555b9254939bb7
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-03-31 17:32:35 +0530

    Add files via upload

---
+++ Natural.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Natural{
+    public static void main(String[] args) {
+        int n ,i,sum=0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the natural number: ");
+        n = in.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            sum = sum + i;
+        }
+        System.out.println("Sum is : "+sum);
+    }
+}
