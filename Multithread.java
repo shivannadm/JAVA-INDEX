f59149d00d5c0610cdfe6bee95a81fba59edd8b7
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-03-31 18:39:34 +0530

    Add files via upload

---
+++ Multithread.java
@@ -0,0 +1,35 @@
+
+class Hello extends Thread{
+    public void run()
+    {
+        for(int i=1;i<5;i++)
+        {
+        System.out.println("Hello");
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e)
+        {}}
+    }
+}
+
+class World extends Thread{
+    public void run()
+    {
+        for(int i=1;i<5;i++){
+        System.out.println("World");
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e)
+        {}}
+    }
+}
+
+public class Multithread{
+    public static void main(String[] args) {
+        Hello ob = new Hello();
+        World ob1 = new World();
+        ob.start();
+        ob1.start();
+    }
+}
+   
