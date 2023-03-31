52ac15a08d9c3365a1f69f35a923fd4f4773236c
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-03-31 18:15:32 +0530

    Add files via upload

---
+++ Vehicleinfo.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Vehicle{
+    String Brand , Price;
+    Scanner in = new Scanner(System.in);
+
+    void read()
+    {
+        System.out.println("Enter the Vehicle brand and price: ");
+        Brand = in.next();
+        Price = in.next();
+    }
+
+    void display()
+    {
+        System.out.println("Vehicle details are: "+Brand+"  "+Price);
+    }
+}
+
+    class Car extends Vehicle{
+        Scanner in = new Scanner(System.in);
+        String modelName,color;
+        void read()
+        {
+            super.read();
+            System.out.println("Enter the model name and color: ");
+            modelName = in.next();
+            color = in.next();
+        }
+        void display()
+        {
+            super.display();
+            System.out.println("Car madel and color is: "+modelName+"  "+color);
+        }
+    }
+
+
+public class Vehicleinfo{
+    public static void main(String[] args) {
+        Car ob = new Car();
+        ob.read();
+        ob.display();
+    }
+}
