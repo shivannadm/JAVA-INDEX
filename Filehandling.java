72dbb9588f4900441ce99d4c621c5bff75545355
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-04-03 19:19:33 +0530

    Create Filehandling.java

---
+++ Filehandling.java
@@ -0,0 +1,29 @@
+import java.io;
+import java.util.*;
+
+public class  Filehandling {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter file to be checked: ");
+        String fname = in.next();
+        File fl = new File(fname);
+        System.out.println(fl.exists()?"Yes":"no");
+        System.out.println(fl.canRead()?"yeah":"no");
+        System.out.println("File path is: ");
+        fl.getAbsolutepath(fl.getAbsolutePath());
+        System.out.println("Size is: "+fl.length());
+        fl.setWritable(false);
+        System.out.println(fl.canWrite()?"S":"no");
+        int index = fname.lastIndexOf(".");
+        
+        if(index>0)
+        {
+            String type = fname.substring(index+1);
+            System.out.println(type);
+        }
+        else{
+            System.out.println("no type");
+        }
+    }
+    
+}
