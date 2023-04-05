df338881cbf92b1ecfee63650cbdcdd72e7bab44
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-04-05 20:12:58 +0530

    Update Exceptin.java

--- Exceptin.java
+++ Exceptin.java
@@ -1,10 +1,11 @@
 public class Exception {
     public static void main(String[] args) {
-        int a[]={1,2,3};
-        String name=null;
-        try{
+        int a[] = {1,2,3};
+        String name = null;
+        
+        try {
             System.out.println(name.length());
-        }catch(NullPointerException e)
+        } catch(NullPointerException e)
         {
             System.out.println(e);
             System.out.println("Name is set to null, please provide valid name:");
