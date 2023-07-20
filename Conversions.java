3844861483b642b218dba206c7d9ae632de057d3
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-07-20 19:19:26 +0530

    Update Conversions.java

--- Conversions.java
+++ Conversions.java
@@ -1,3 +1,6 @@
+
+//package building
+
 import currencyconversion.*; //You need to creat one package for currence conversion , which I uploaded the file name is Currency.jav 
 import distanceconversion.*;  //You need to creat package for currence conversion , which I uploaded the file name is Distance.java
 import timeconversion.*;  //You need to creat package for currence conversion , which I uploaded the file name is Time.java
@@ -23,3 +26,4 @@ public class Conversions {
         obj2.secondstohours();
     }
 }
+
