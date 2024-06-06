94fbef422a6712de69feffa1681d2d36402c3444
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2024-06-06 18:46:30 +0530

    Add files via upload

---
+++ Abstract/Main.java
@@ -0,0 +1,34 @@
+package Abstract;
+
+abstract class Car{
+    public abstract void drive();
+    public abstract void fly();
+
+    public void musicPlay(){
+        System.out.println("Music Playing...");
+    }
+    
+}
+
+abstract class WagonR extends Car{
+    public void drive(){
+        System.out.println("Driving...");
+    }
+    
+}
+
+class UpdatedWagonR extends WagonR{
+    public void fly(){
+        System.out.println("Flying...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car obj = new UpdatedWagonR();
+        obj.drive();
+        obj.musicPlay();
+        obj.fly();
+
+    }
+}
