2fb752bc7902985ea699ec9fbd264b9ec96c00b7
shivannadm <shivannadm6@gmail.com>
2025-09-18 21:02:31 +0530

    Update 396rotate.java

--- LeetCode/396rotate.java
+++ LeetCode/396rotate.java
@@ -1,26 +1,19 @@
-// Not Optimized
+// Optimezed Code (396. Rotate Function)
 class Solution {
     public int maxRotateFunction(int[] nums) {
         int n = nums.length;
-        int max = 0;
+        int s1 = 0;
+        int s2 = 0;
         for(int i=0;i<n;i++){
-            max = max + (i*nums[i]);
+            s1 += nums[i];
+            s2 += i*nums[i];
         }
 
-        
+        int res = s2;
         for(int i=0;i<n;i++){
-            int m2=0;
-            for(int j=n-1;j>0;j--){
-                int temp = nums[j];
-                nums[j] = nums[j-1];
-                nums[j-1] = temp;
-            }
-            
-            for(int k=0;k<n;k++){
-                m2 = m2+(k*nums[k]);
-            }
-            max = Math.max(max,m2);
+            s2=s2-s1+(n)*nums[i];
+            res = Math.max(res,s2);
         }
-        return max;
+        return res;
     }
 }
