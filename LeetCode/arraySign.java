161d20e1a1c7a493938bd72f2c401195546ebd71
Shivanna <119150988+shivannadm@users.noreply.github.com>
2025-09-20 18:28:52 +0530

    Create arraySign.java

---
+++ LeetCode/arraySign.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        for(int num:nums){
+            product *= num;
+        }
+        if(product >0){
+            return 1;
+        }else if(product==0){
+            return 0;
+        }
+        return -1;
+    }
+}
